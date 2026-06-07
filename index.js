48cd2fbcec247c715eb5027279592a87684293ef
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-07 23:37:06 +0530

    userId: route

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 const express = require('express')
 const app = express()
+
 app.get('/', (req,res) => {
     res.send('Hello from express')
 })
-app.listen(3000, () => {
-    console.log('Server running on port 3000')
-})
+
 app.get('/about', (req,res) => {
     res.json({ name: "Sanjana", role: "Full Stack Developer" })
 })
+
 app.get('/health', (req,res) => {
     res.json({ status: "ok" })
+})
+
+app.get('/user/:id', (req,res) => {
+    res.json( {userId: req.params.id} )
+})
+
+
+
+app.listen(3000, () => {
+    console.log('Server running on port 3000')
 })
