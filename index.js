daed56f907e3b2594532ee5371af1a59263b3c29
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-23 23:53:08 +0530

    middleware

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+app.use((req, res, next) => {
+    console.log(`${req.method} ${req.url}`)
+    next()
+})
+
 app.get('/', (req,res) => {
     res.send('Hello from express')
 })
