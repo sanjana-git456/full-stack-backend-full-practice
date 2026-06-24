f4b0d728090359e73b04217348b6b3bc118dee9a
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-24 22:45:59 +0530

    error handling with status codes

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/user/:id', (req,res) => {
+    const id = req.params.id
+
+    if (id === '0') {
+        return res.status(404).json({ error: "User not found" })
+    }
+    
     res.json( {userId: req.params.id} )
 })
 
