071c4af8f4365fdf36381371534f75f3531216b0
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-25 02:05:51 +0530

    post /register route data entry in mongodb

--- routes/auth.js
+++ routes/auth.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const User = require('../models/User')
 
 router.post('/login', (req, res) => {
     res.json( {message: `Welcome, ${req.body.username}`} )
+})
+
+router.post('/register', async (req,res,next) => {
+    try {
+        const { name, email, password } = req.body
+        const user = new User({ name, email, password })
+        await user.save()
+
+        res.status(201).json({ messgae: 'User created', user })
+    } catch (err) {
+        next(err)
+    }
 })
 
 module.exports = router
