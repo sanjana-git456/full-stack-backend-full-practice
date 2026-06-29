1cf2c588594ffa60aa8daa5f5d72b099846a8b6a
Sanjana Khajuria <sanjana.newgit@gmail.com>
2026-06-29 20:01:05 +0530

    added cors middleware

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express')
 const app = express()
 app.use(express.json())
 
+const cors = require('cors')
+app.use(cors())
+
 app.use((req, res, next) => {
     console.log(`${req.method} ${req.url}`)
     next()
